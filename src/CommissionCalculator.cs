65211802e43c062b9410415180bff071bd7882b3
viniciusOP <viniciusopassos@hotmail.com>
2022-10-24 20:55:59 -0300

    feat: metodo ShowFinalSalary com mensagem final ao usuario

--- src/CommissionCalculator.cs
+++ src/CommissionCalculator.cs
@@ -12,6 +12,8 @@ namespace CommissionCalculation
         public int AmountCarsSold { get; set; }
         public decimal TotalSalesValue { get; set; }
         public decimal FinalSalary { get; set; }
+        public decimal FixedCommission { get; set; }
+        public decimal CommissionPerSale { get; set; }
 
         public decimal CalculateFinalSalary(decimal fixedSalary, int amountCarsSold, decimal totalSalesValue)
         {
@@ -24,6 +26,18 @@ namespace CommissionCalculation
             FinalSalary = fixedSalary + fixedCommission + commissionPerSale;
 
             return FinalSalary;
+        }
+
+        public string[] ShowFinalSalary(string contributorName, string month)
+        {
+            return new string[]
+            {" O colaborador " + contributorName + " neste mês de " + month + " obteve o salário final de R$ " + FinalSalary + " referente à: ", 
+            " SALÁRIO FIXO: R$ " + FixedSalary,
+            " TOTAL DE CARROS VENDIDOS: " + AmountCarsSold,
+            " VALOR TOTAL DE VENDAS NO MÊS: R$ " + TotalSalesValue,
+            " COMISSÃO POR CARROS VENDIDOS: R$ " + FixedCommission,
+            " COMISSÃO DE 3% DO TOTAL DE VENDAS: R$ " + CommissionPerSale,
+            }; 
         }
     }
 }
